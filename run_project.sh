6da04544f1e24741d223fce353685a5d28700ff1
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-05-30 00:44:53 +0200

    ci: change host to 0.0.0.0

--- run_project.sh
+++ run_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the first process
-uvicorn api:app --reload &
+uvicorn api:app --reload --host 0.0.0.0 &
 # Start the second process
 flask --app main run --debug --port 6000 --host 0.0.0.0 &
 # Wait for any process to exit
